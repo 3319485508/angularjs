72093f522aeeb3f9da26c4682b6936cd91f6cdee
3319485508 <3319485508@qq.com>
2016-01-05 16:47:17 +0800

    2016.01.05 添加到Git

---
+++ js/controller.007.js
@@ -0,0 +1,19 @@
+var myApp = angular.module("myApp", [])
+				   .controller("myController", function($scope){
+					   var technologies = [
+							{ name: "C#", like: 0, dislike: 0 },
+							{ name: "ASP.NET", like: 0, dislike: 0 },
+							{ name: "SQL", like: 0, dislike: 0 },
+							{ name: "AngularJS", like: 0, dislike: 0 }
+					   ];
+					   
+					   $scope.technologies = technologies;
+					   
+					   $scope.incrementLike = function(technology){
+						   technology.like++;
+					   };
+					   
+					   $scope.incrementDislike = function(technology){
+						   technology.dislike++;
+					   }
+				   });
---
+++ js/controller.008.js
@@ -0,0 +1,52 @@
+var myApp = angular.module("myApp", [])
+				   .controller("Empolyees", function($scope){
+					   
+					   var employees = [
+						{
+							name: "Ben",
+							birthday: new Date("November 23, 1980"),
+							gender: "Male",
+							salary: 55000.788
+						},
+						{
+							name: "Sara",
+							birthday: new Date("May 05, 1970"),
+							gender: "Female",
+							salary: 68000
+						},
+						{
+							name: "Mark",
+							birthday: new Date("August 15, 1974"),
+							gender: "Male",
+							salary: 57000
+						},
+						{
+							name: "Pam",
+							birthday: new Date("October 27, 1979"),
+							gender: "Female",
+							salary: 53000
+						},
+						{
+							name: "Todd",
+							birthday: new Date("December 30, 1983"),
+							gender: "Male",
+							salary: 60000
+						},
+						{
+							name: "Ben",
+							birthday: new Date("November 23, 1980"),
+							gender: "Male",
+							salary: 55000.788
+						}
+					   ];
+					   
+					   $scope.employees = employees;
+					   $scope.rowCount = 3;
+					   $scope.sortColumn = "name";
+					   $scope.reverseSort = false;
+					   
+					   $scope.sortData = function(column){
+						   $scope.reverseSort = ($scope.sortColumn == column) ? !$scope.reverseSort : false;
+						   $scope.sortColumn = column;
+					   }
+				   })
